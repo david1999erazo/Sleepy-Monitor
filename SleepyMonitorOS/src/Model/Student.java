1d918a07c488b0b58c20b029560472578964d8ce
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-03-16 18:42:56 -0500

    Add commnets to the student class

--- SleepyMonitorOS/src/Model/Student.java
+++ SleepyMonitorOS/src/Model/Student.java
@@ -36,16 +36,22 @@ public class Student extends Thread {
 	public void run() {
 		System.out.println("The student was create, in this moment thes student is programming in the computer room");
 		
-		//while(true) {
-			
-			
 			try {
-				sleep((random.nextInt(16)+30)*1000);
+				
+				/**
+				 * This line sleeps the thread between xxx to xxx seconds.
+				 */
+				sleep((random.nextInt(12)+26)*1000);
 				System.out.println("A student will try to ask for help from the monitor");
 				
+				/**
+				 * This condition check if the monitor is busy
+				 */
 				if(semaphoreMonitor.availablePermits()==0) {
 					
-					
+					/**
+					 * This condition check if the chairs are available
+					 */
 					if(semaphoreWaitingRoom.availablePermits()==0) {
 						
 						System.out.println("The monitor is busy and there are no chairs in the waiting room ");
@@ -53,21 +59,37 @@ public class Student extends Thread {
 						
 					}else {
 						
+						/**
+						 * This line represents when the student sits in a chair in the waiting room
+						 */
 						semaphoreWaitingRoom.acquire();
 						
 						System.out.println("The monitor is busy, so the student sits in one of the waiting chairs");
 						
 						System.out.println("Chairs avaible: "+ semaphoreWaitingRoom.availablePermits());
 					
+						/**
+						 * This line represents when the student waiting the monitor
+						 */
 						semaphoreMonitor.acquire();
+						/**
+						 * This line represents the chair empty in the waiting room
+						 */
 						semaphoreWaitingRoom.release();
 						
 						System.out.println("A student enters the monitor's office");
 						
 						System.out.println("Chairs avaible: "+ semaphoreWaitingRoom.availablePermits());
 						
-						sleep((random.nextInt(11)+15)*1000);
+						/**
+						 * This line sleeps the thread between 30 to 40 seconds.
+						 * This represent the time the monitor helps the student
+						 */
+						sleep((random.nextInt(16)+30)*1000);
 						
+						/**
+						 * This line represent when the monitor is free
+						 */
 						semaphoreMonitor.release();
 						
 						System.out.println("The monitor resolved the student's question");
@@ -81,17 +103,30 @@ public class Student extends Thread {
 					System.out.println("The monitor is asleep");
 					System.out.println("A student wakes up the monitor");
 					
+					/**
+					 * This line represents when the student wake up the monitor 
+					 */
 					semaphoreMonitor.acquire();
 					
 					System.out.println("A student enters the monitor's office");
 					
-					sleep((random.nextInt(11)+15)*1000);
+					/**
+					 * This line sleeps the thread between 30 to 40 seconds.
+					 * This represent the time the monitor helps the student
+					 */
+					sleep((random.nextInt(16)+30)*1000);
 					
+					/**
+					 * This line represent when the monitor is free
+					 */
 					semaphoreMonitor.release();
 					
 					System.out.println("The monitor resolved the student's question");	
 				}
 				
+				/**
+				 * This condition check if the monitor is free and there is no student in the waiting room
+				 */
 				if(semaphoreWaitingRoom.availablePermits()==3 && semaphoreMonitor.availablePermits()==1) {
 					System.out.println("The monitor can go back to sleep");
 				}
@@ -99,7 +134,6 @@ public class Student extends Thread {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		//}
 	}
 	
 	
